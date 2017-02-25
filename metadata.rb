e2d050fc63e6b2f2dc51770728650ad15cadb023
Roberto Leibman <roberto@leibman.net>
2017-02-24 16:42:25 -0800

    Done

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sinfomicien@gmail.com'
 license 'Apache 2.0'
 description 'Provides the mysql2_chef_gem resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.0'
+version '1.2.1'
 
 supports 'amazon'
 supports 'redhat'
@@ -16,7 +16,7 @@ supports 'ubuntu'
 supports 'oracle'
 
 depends 'build-essential'
-depends 'mysql',               '~> 8.0.1'
+depends 'mysql',               '~> 8.2.0'
 depends 'mariadb'
 
 source_url 'https://github.com/sinfomicien/mysql2_chef_gem' if respond_to?(:source_url)
